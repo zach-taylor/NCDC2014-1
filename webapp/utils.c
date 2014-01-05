2a7151f8b3c014059f66c1a8202c91c3783a0753
CDC <cdc@ubuntu.(none)>
2014-01-04 21:04:56 -0800

    cool stuff

--- webapp/utils.c
+++ webapp/utils.c
@@ -1,19 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 char* read_file(char* filename)
 {
-//    FILE* file = fopen(filename,"r");
-//    if(file == NULL)
-//    {
-//        return NULL;
-//    }
-//
-//    fseek(file, 0, SEEK_END);
-//    long int size = ftell(file);
-//    rewind(file);
-//
-//    char* content = calloc(size + 1, 1);
-//
-//    fread(content,1,size,file);
-//
-//    return content;
-  return "TODO";
+    FILE* file = fopen(filename,"r");
+    if(file == NULL)
+    {
+        return NULL;
+    }
+
+    fseek(file, 0, SEEK_END);
+    long int size = ftell(file);
+    rewind(file);
+
+    char* content = calloc(size + 1, 1);
+
+    fread(content,1,size,file);
+
+    return content;
 }
